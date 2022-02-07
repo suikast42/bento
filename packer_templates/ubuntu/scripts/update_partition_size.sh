ab24ec1e3191f84d9c5e20d6f7e3c5be213d46d8
VURU <sueleyman.vurucu@amova.eu>
2022-02-07 18:20:05 +0100

    auto size

--- packer_templates/ubuntu/scripts/update_partition_size.sh
+++ packer_templates/ubuntu/scripts/update_partition_size.sh
@@ -2,8 +2,8 @@
 
 echo "Full disk capacity for /dev/sda3"
 # Increase the Physical Volume (pv) to max size
-pvresize /dev/sda3
+sudo pvresize /dev/sda3
 # Expand the Logical Volume (LV) to max size to match
-lvresize -l +100%FREE /dev/mapper/ubuntu--vg-ubuntu--lv
+sudo lvresize -l +100%FREE /dev/mapper/ubuntu--vg-ubuntu--lv
 # Expand the filesystem itself
-resize2fs /dev/mapper/ubuntu--vg-ubuntu--lv
+sudo resize2fs /dev/mapper/ubuntu--vg-ubuntu--lv

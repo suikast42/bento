ee3e38075cdec5cd19e4786e50c672970b9eded4
VURU <sueleyman.vurucu@amova.eu>
2022-02-07 19:12:48 +0100

    lvresize not works

--- packer_templates/ubuntu/scripts/update_partition_size.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -eux
-
-echo "Full disk capacity for /dev/sda3"
-# Increase the Physical Volume (pv) to max size
-sudo pvresize /dev/sda3
-# Expand the Logical Volume (LV) to max size to match
-sudo lvresize -l +100%FREE /dev/mapper/ubuntu--vg-ubuntu--lv
-# Expand the filesystem itself
-sudo resize2fs /dev/mapper/ubuntu--vg-ubuntu--lv
